5e0946ed0f70d6a5a68394ede855d07f341938ce
Megumi Kurihara <pomme.c5.musique@gmail.com>
2021-08-28 13:14:20 +0900

    modified station5

--- handler/healthz.go
+++ handler/healthz.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/TechBowl-japan/go-stations/model"
@@ -16,5 +17,7 @@ func NewHealthzHandler() *HealthzHandler {
 
 // ServeHTTP implements http.Handler interface.
 func (h *HealthzHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	_ = &model.HealthzResponse{}
+	response := &model.HealthzResponse{}
+	response.Message = "OK"
+	json.NewEncoder(w).Encode(response)
 }
